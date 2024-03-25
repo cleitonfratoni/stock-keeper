8c86846f6cda0c7836a45a4873c18058d7e307fa
mathgoms02 <math.gomsx3@gmail.com>
2024-03-25 14:22:36 -0300

    Finished the Log-in Screen

--- assets/css/Css.js
+++ assets/css/Css.js
@@ -1,38 +1,82 @@
 import { StyleSheet } from "react-native";
 
 const css = StyleSheet.create({
-    // container: {
-    //   flex: 1,
-    //   backgroundColor: '#fff',
-    //   paddingLeft: 10,
-    // },
     container_fundo:{
       backgroundColor: '#000',
       alignItems: 'center',
     },
+
     button_home:{
       height: 70,
       width: 70,
     },
+
     img_logo:{
       height: 100,
       width: 120,
-      justifyContent: 'center',
+      marginLeft: 100,
+      marginBottom: 30,
     },
+
+    container_textinput:{
+      padding: 20,
+      // margin: 30,
+    },
+
     textPage_login:{
         height:15,
         color: 'white',
         fontWeight: "bold",
         marginRight: 250,
-        // padding:20,
+        marginLeft: 5,
     },
+
     text_input:{
-        height: 35,
+        height: 40,
         width:325,
         margin: 3,
         padding: 10,
         backgroundColor: 'white',
-        borderRadius: 15,
-    }
+        borderRadius: 12,
+    },
+
+    // container_button:{
+    //   height: 35,
+    //   width:325,
+    //   color: '#000',
+    // },
+    container_fundo_2:{
+      backgroundColor: '#000',
+      alignItems: 'center',
+      padding: 30,
+    },
+
+    container_button_2:{
+      backgroundColor: '#fff',
+      borderRadius: 30,
+      paddingVertical: 10,
+      paddingHorizontal: 40,
+      marginVertical: 8,
+      width: 320,
+      height:40,
+    },
+    text_button:{
+      color: '#000',
+      fontSize: 18,
+      textAlign: 'center',
+    },
+    text_esqueceu_senha:{
+      height: 15,
+      width: 130,
+      color: 'white',
+      textDecorationLine: 'underline',
+      marginRight: 25,
+      marginLeft: 5,
+    },
+    container_footer:{
+      backgroundColor: '#000',
+      alignItems: 'center',
+      padding: 110,
+    },
   });
 export{css};
--- views/Login.js
+++ views/Login.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Text, View, StyleSheet, Button, Image, TextInput } from 'react-native';
+import { Text, View, StyleSheet, Button, Image, TextInput, TouchableOpacity } from 'react-native';
 import { css } from '../assets/css/Css';
 
 
@@ -7,21 +7,41 @@ import { css } from '../assets/css/Css';
 export default function Login(){
     return(
         <View style={css.container_fundo}>
-            <Image style={css.img_logo} source={require('../assets/img/SK_mini.png')}/>
-            <Text style={css.textPage_login}>Username</Text>
-            <TextInput
-                style={css.text_input}
-                placeholder=""
-                onChangeText={(text) => console.log(text)}
-            />
-            <Text style={css.textPage_login}>Senha</Text>
-            <TextInput
-                style={css.text_input}
-                placeholder=""
-                onChangeText={(text) => console.log(text)}
-            />
-            <Button title='Botão' 
-            onPress={()=>route.navigation.navigate('Home',{id:31})}/>
+            <View style={css.container_textinput}>
+                <Image style={css.img_logo} source={require('../assets/img/SK_mini.png')}/>
+                <Text style={css.textPage_login}>Username</Text>
+                <TextInput
+                    style={css.text_input}
+                    placeholder=""
+                    onChangeText={(text) => console.log(text)}
+                />
+            </View>
+            <View style={css.container_textinput}>
+                <Text style={css.textPage_login}>Senha</Text>
+                <TextInput
+                    style={css.text_input}
+                    placeholder=""
+                    onChangeText={(text) => console.log(text)}
+                />
+                <Text onPress={()=>route.navigation.navigate('Home',{id:31})} style={css.text_esqueceu_senha}>Esqueceu a Senha?</Text>
+            </View>
+            <View style={css.container_fundo_2}>
+                {/* <Button style={css.container_button} 
+                title='Entrar' 
+                color='#fff'
+                onPress={()=>route.navigation.navigate('Home',{id:31})}/> */}
+                
+                <TouchableOpacity style={css.container_button_2} onPress={()=>route.navigation.navigate('Home',{id:31})}>
+                    <Text style={css.text_button}>Entrar</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={css.container_button_2} onPress={()=>route.navigation.navigate('Home',{id:31})}>
+                    <Text style={css.text_button}>Cadastre-se</Text>
+                </TouchableOpacity>
+                <View style={css.container_footer}>
+                </View>
+
+            </View>
         </View>
 
     )
